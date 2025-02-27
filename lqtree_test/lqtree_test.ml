8367912d52dc35d7b5a408070bc934098a1f007f
Liam Berry <liamandberry@gmail.com>
2025-02-27 15:14:20 +0200

    insert test

--- lqtree_test/lqtree_test.ml
+++ lqtree_test/lqtree_test.ml
@@ -152,6 +152,87 @@ let test_insert2 () =
   done
 ;;
 
+(*
+   Insert into Node:
+              ┌──────┐                      
+              │ Root │                      
+   ┌──────────┤ 2 2;2├──────────┐           
+   │          ├──────┤          │           
+   │          │      │          │           
+ ┌─▼──┐    ┌──▼─┐   ┌▼───┐   ┌──▼─┐         
+ │ UL │    │ UR │   │ LL │   │ LR │         
+ └────┘    ├────┘   └─┬──┘   └────┘         
+           ▼          ▼                     
+    1 (2.5;2.5)   1 (1.5;1.5)               
+                  │                         
+                  │                         
+                  │                         
+                  │   1 (2.5;1.5)           
+                  │                         
+                  │                         
+                  │                         
+                  ▼                         
+              ┌──────┐                      
+              │ Root │                      
+   ┌──────────┤ 3 2;2├──────────┐           
+   │          ├──────┤          │           
+   │          │      │          │           
+ ┌─▼──┐    ┌──▼─┐   ┌▼───┐   ┌──▼─┐         
+ │ UL │    │ UR │   │ LL │   │ LR │         
+ └────┘    ├────┘   └─┬──┘   └────┘         
+           ▼          ▼                     
+    1 (2.5;2.5)   1 (1.5;1.5)   1 (2.5;1.5)
+*)
+
+let test_insert3 () =
+  let open Lqtree in
+  let qt_test =
+    let open Quadrant in
+    let open Node in
+    let open Qtree in
+    let ul = new_node zero_c (to_bbox UL test_bbox) in
+    ul.next <- 2;
+    let ur = new_node (1.0, (2.5, 2.5)) (to_bbox UR test_bbox) in
+    ur.next <- 3;
+    let ll = new_node (1.0, (1.5, 1.5)) (to_bbox LL test_bbox) in
+    ll.next <- 4;
+    let lr = new_node (1.0, (2.5, 1.5)) (to_bbox LR test_bbox) in
+    lr.next <- 0;
+    let t = { nodes = Dynarray.create () } in
+    let root =
+      new_node
+        (centroid_sum
+           (centroid_sum (1.0, (1.5, 1.5)) (1.0, (2.5, 2.5)))
+           (1.0, (2.5, 1.5)))
+        test_bbox
+    in
+    root.children <- 1;
+    Dynarray.add_last t.nodes root;
+    Dynarray.add_last t.nodes ul;
+    Dynarray.add_last t.nodes ur;
+    Dynarray.add_last t.nodes ll;
+    Dynarray.add_last t.nodes lr;
+    t
+  in
+  let qt_node =
+    let qt = Qtree.{ nodes = Dynarray.create () } in
+    let node = Node.new_node (1.0, (1.5, 1.5)) test_bbox in
+    Dynarray.add_last qt.nodes node;
+    qt
+  in
+  Qtree.insert qt_node (1.0, (2.5, 2.5));
+  Qtree.insert qt_node (1.0, (2.5, 1.5));
+  Printf.printf "lead: %s\n" (Qtree.sexp_of_t qt_node |> Sexplib.Sexp.to_string_hum);
+  for i = 0 to Dynarray.length qt_test.nodes - 1 do
+    let example_node = Dynarray.get qt_test.nodes i in
+    let node = Dynarray.get qt_node.nodes i in
+    Alcotest.(check bool)
+      ("Node " ^ string_of_int i ^ " are the same")
+      true
+      (Node.equal node example_node)
+  done
+;;
+
 let () =
   let open Alcotest in
   run
@@ -163,6 +244,7 @@ let () =
     ; ( "insert"
       , [ test_case "Insert into empty" `Quick test_insert1
         ; test_case "Insert into leaf" `Quick test_insert2
+        ; test_case "Insert into node" `Quick test_insert3
         ] )
     ]
 ;;
